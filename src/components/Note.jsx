1ae751923eec773d0246cf80389179cec2a49f12
tabbytadi <adi_stankova@abv.bg>
2025-07-11 17:56:34 +0300

    Keeper App Project

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div class="note">
+      <h1>Title</h1>
+      <p>This is some note content.</p>
+    </div>
+  );
+}
+
+export default Note;
